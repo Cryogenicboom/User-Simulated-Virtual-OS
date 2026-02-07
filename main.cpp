9a81bbf7e3813072f2667117276481514bccc986
Aryan Mishra <aryanxposat@gmail.com>
2026-02-07 09:19:05 +0530

    Calculate time_remaining after time_remaining--
    instead of using "return 0" inside case::running, use break.
    calculate time remaining after time_remaining--;
    cpu_time_used = total time given - remaining time

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Every type of TIME is always calculated as a whole number, considering "0" as your starting point
+
 #include <iostream>
 #include <string>
 #include "process.h"
@@ -18,19 +20,18 @@ const char* Process_state(ProcessState state){
 }
 
 int main(){
-    int global_time = 1;
-    
     Process process1(101, 4, 0);
 
-    // std::cout << process1.pid<<"\n";
+    int global_time = 1;
 
+    // std::cout << process1.pid<<"\n";
     // // to print custom "enum" class, convert explicity it to integer then access its element
     // // int state_index = static_cast<int>(process1.state);
     // Process_state(process1.state);
 
     // std::cout << process1.cpu_time_total;
     std::cout<<"Tick = 0\n";
-    std::cout<<Process_state(process1.state)<<"\n";
+    std::cout<<Process_state(process1.state)<<" is Initial State.\n";
     while(process1.state != ProcessState :: Finished){
         switch (process1.state) {
             case ProcessState::New:
@@ -40,18 +41,21 @@ int main(){
                 process1.state = ProcessState :: Running;
                 break;
             case ProcessState :: Running:
+                process1.cpu_time_remaining -= 1;
                 if(process1.cpu_time_remaining == 0){
                     process1.state = ProcessState :: Finished;
-                    return 0;
+                    break;
                 }
                 break;
         }
-        std::cout<<"Tick = "<<global_time;
+        int cpu_used = process1.cpu_time_total - process1.cpu_time_remaining;
+
+        std::cout<<"\nTick = "<<global_time;
         std::cout<<"\n";
         std::cout<<Process_state(process1.state)<<"\n";
-        process1.cpu_time_remaining -= 1;
         global_time ++;
-        
+        std::cout<<"cpu time used = "<<cpu_used<<"\n";
+        // std::cout<<"\n";
         }
     
     return 0; 
